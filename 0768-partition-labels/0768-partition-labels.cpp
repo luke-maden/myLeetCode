7eafc30131ae2d2f6cd11b360a2ad02588a7dc64
Luke Maden <30941461+luke-maden@users.noreply.github.com>
2025-03-30 12:41:13 -0700

    0768-PARTITION-LABELS Stats: Time: 0 ms (100%), Space: 8.8 MB (88.16%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.cpp
+++ 0768-partition-labels/0768-partition-labels.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        unordered_map<char, bool> seen;
+        unordered_map<char, int> lastInd;
         vector<int> result;
         int ind1 = 0;
         int ind2 = 0;
-        unsigned long lim1 = s.length();
+        int lim1 = s.length();
+
+        for(int i = 0; i < lim1; i++){
+            lastInd[s[i]] = i;
+        }
 
         while(ind1 < s.length()){
-            lim1 = s.find_last_of(s.at(ind1));
+            lim1 = lastInd[s[ind1]];
             ind2 = ind1 + 1;
-            seen[s.at(ind1)] = true;
 
             while(ind2 < lim1){
-                if(!seen[s.at(ind2)]){
-                    lim1 = max(lim1, s.find_last_of(s.at(ind2)));
-                    seen[s.at(ind2)] = true;
-                }
-
+                lim1 = max(lim1, lastInd[s[ind2]]);
                 ind2++;
             }
 
