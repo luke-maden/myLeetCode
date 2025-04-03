af06402b1c28f6deb311f78a2d1372e5abffea8c
Luke Maden <30941461+luke-maden@users.noreply.github.com>
2025-04-02 22:38:18 -0700

    0136-SINGLE-NUMBER Stats: Time: 0 ms (100%), Space: 46.8 MB (46.29%) - LeetHub

--- 0136-single-number/0136-single-number.cs
+++ 0136-single-number/0136-single-number.cs
@@ -1,16 +1,11 @@
 public class Solution {
     public int SingleNumber(int[] nums) {
-        if(nums.Length == 1){
-            return nums[0];
-        }
-        Array.Sort(nums);
+        int result = 0;
 
-        for(int i = 0; i < nums.Length - 1; i += 2){
-            if(nums[i] != nums[i + 1]){
-                return nums[i];
-            }
+        foreach(int num in nums){
+            result ^= num;
         }
 
-        return nums[nums.Length - 1];
+        return result;
     }
 }
